4bc2ab0cca2c33b98b9c1960a6ef5e97daa6b93f
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-01 14:04:40 -0700

    WIP: Building UI logic

--- src/currency.js
+++ src/currency.js
@@ -1,12 +1,13 @@
 export default class CurrencyExchanger {
-  static getCurrency(){
+  static getCurrency(Code,inputAmount){
     return fetch(
-      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${Code}/${inputAmount}`
     )
       .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
+        console.log(response);
         return response.json();
       })
       .catch(function (error) {
@@ -14,3 +15,4 @@ export default class CurrencyExchanger {
       });
   }
 }
+
--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import CurrencyExchanger from "./js/currency.js";
+import CurrencyExchanger from "./currency.js";
 
 function getElements(response) {
-  if (response) {
-    let countryCode = $("#currencies").val();
+  if (response["conversion_rates"]) {
+    let Code = $("#currencies").val();
     let inputAmount = $("#amountInput").val();
     $(".input").text(inputAmount);
-    $("#output").text(
-      `${
-        response["conversion_rates"]} * ${response["conversion_rates"].countryCode
-      }`
-    );
+    $("#output").text(response["conversion_rates"][`"${Code}"`]);
   } else {
     $(".showErrors").text(`There was an error: ${response.message}`);
   }
@@ -21,7 +17,7 @@ function getElements(response) {
 
 $(document).ready(function () {
   $("#covertButton").submit(function () {
-    CurrencyExchanger.getCurrency().then(function (response) {
+    CurrencyExchanger.getCurrency(Code,inputAmount).then(function (response) {
       getElements(response);
     });
   });
